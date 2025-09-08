0e1ce519ae19f27b7abfbc8d1be3035d05556126
Asegid Adane <142203006+ASEGIDADANE@users.noreply.github.com>
2025-09-08 12:22:07 +0300

    Time: 27 ms (42.55%) | Memory: 29 MB (38.38%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
+        diff = [0] * (n + 1)
+        for f, l, s in bookings:
+            diff[f - 1] += s
+            diff[l] -= s
+            # print(diff)
+        for i in range(1, n):
+            diff[i] += diff[i - 1]
+        return diff[:-1]
